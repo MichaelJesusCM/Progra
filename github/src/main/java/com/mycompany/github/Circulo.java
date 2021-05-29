b1b9f817efe92b4ced1bc4c29c83e8b73741bbb4
USUARIO <USUARIO@LAPTOP-8K0E208O>
2021-05-28 19:42:18 -0500

    Cambio Edison

---
+++ github/src/main/java/com/mycompany/github/Circulo.java
@@ -0,0 +1,21 @@
+package com.mycompany.github;
+
+public class Circulo {
+
+    private double area;
+    private double perimetro;
+
+    public Circulo(Double a, Double b) {
+        this.area = a;
+        this.perimetro = b;
+    }
+
+    public void area() {
+        System.out.println("El area de circulo es: ");
+    }
+
+    public void perimitro() {
+        System.out.println("El perimetro del circulo es:");
+
+    }
+}
