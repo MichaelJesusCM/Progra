fcdd4f8ff0ac49514213765ac5e593778773958e
Michael Jesús <Michael Jess@192.168.56.1>
2021-06-01 18:17:26 -0500

    //continuacion de los casos

--- github/src/main/java/com/mycompany/github/proyecto1.java
+++ github/src/main/java/com/mycompany/github/proyecto1.java
@@ -20,58 +20,86 @@ public class proyecto1 {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
-    
 
-    
-        try{
-     
+        try {
 
-        System.out.println("seleccione la operacion a realzar\n 1.-Area y permetro del Cuadrado.");
-        System.out.println("seleccione la operacion a realzar\n 2.-Area y permetro del Rectangulo.");
-        System.out.println("seleccione la operacion a realzar\n 3.-Area y permetro del Triángulo.");
-        System.out.println("seleccione la operacion a realzar\n 4.-Area y permetro del Rombo.");
-        System.out.println("seleccione la operacion a realzar\n 5.-Area y permetro del Romboide.");
-        System.out.println("seleccione la operacion a realzar\n 6.-Area y permetro del Trapecio.");
-        System.out.println("seleccione la operacion a realzar\n 7.-Area y permetro del Circulo.");
-        System.out.println("seleccione la operacion a realzar\n 8.-Area y permetro del Poligono.");
-        opcion = sc.nextInt();
+            System.out.println("seleccione la operacion a realzar\n 1.-Area y permetro del Cuadrado.");
+            System.out.println("seleccione la operacion a realzar\n 2.-Area y permetro del Rectangulo.");
+            System.out.println("seleccione la operacion a realzar\n 3.-Area y permetro del Triángulo.");
+            System.out.println("seleccione la operacion a realzar\n 4.-Area y permetro del Rombo.");
+            System.out.println("seleccione la operacion a realzar\n 5.-Area y permetro del Romboide.");
+            System.out.println("seleccione la operacion a realzar\n 6.-Area y permetro del Trapecio.");
+            System.out.println("seleccione la operacion a realzar\n 7.-Area y permetro del Circulo.");
+            System.out.println("seleccione la operacion a realzar\n 8.-Area y permetro del Poligono.");
+            opcion = sc.nextInt();
 
-        switch (opcion) {
-            case 1:
-                System.out.println("ingrese el lado");
-                lado = sc.nextDouble();
-                Cuadrados cua = new Cuadrados(lado);
-                cua.area();
-                cua.perimetro();
-                break;
+            switch (opcion) {
+                case 1:
+                    System.out.println("ingrese el lado");
+                    lado = sc.nextDouble();
+                    Cuadrados cua = new Cuadrados(lado);
+                    cua.area();
+                    cua.perimetro();
+                    break;
 
-            case 2:
-                System.out.println("ingrese la base");
-                System.out.println("ingrese la altura");
-                base = sc.nextDouble();
-                altura = sc.nextDouble();
-                Rectangulo rec = new Rectangulo(base, altura);
-                rec.area();
-                rec.perimetro();
-                break;
-
-        }
-    }catch(ArithmeticException e){
+                case 2:
+                    System.out.println("ingrese la base");
+                    System.out.println("ingrese la altura");
+                    base = sc.nextDouble();
+                    altura = sc.nextDouble();
+                    Rectangulo rec = new Rectangulo(base, altura);
+                    rec.area();
+                    rec.perimetro();
+                    break;
+                case 3:
+                    double suma=0;
+                    System.out.println("Ingrese la Altura");
+                    System.out.println("Ingrese la Base");
+                    altura = sc.nextDouble();
+                    base = sc.nextDouble();
+                    Triangulo tri=new Triangulo(altura,base,lado);
+                    tri.area();
+                    for(int cont=0;cont<3;cont++){
+                    System.out.println("Ingrese El Lado");
+                    lado = sc.nextDouble();
+                    suma=suma+lado;
+                    
+                    }
+                    break;
+                case 4:
+                     System.out.println("aun no esta listo");
+                    
+                    break;
+                 case 5:
+                     System.out.println("aun no esta listo");
+                    
+                    break;    
+                 case 6:
+                     System.out.println("aun no esta listo");
+                    
+                    break;
+                 case 7:
+                     System.out.println("aun no esta listo");
+                    
+                    break;  
+                case 8:
+                     System.out.println("aun no esta listo");
+                    
+                    break;   
+            }
+        } catch (ArithmeticException e) {
             System.out.println("No se puede dividir para cero.");
             System.out.println(e.getMessage());
             e.printStackTrace();
-        }
-        catch(Exception e){
-        
+        } catch (Exception e) {
+
             System.out.println("Error comuniquese con TIC's");
             System.out.println(e.getMessage());
             e.printStackTrace();
-        }
-                finally{
+        } finally {
             //cerrar la conexion 
             System.out.println("Grcias por utilizar nuestro sericio.");
         }
 
-     }   
     }
-
+}
