5d253344d30dc747ace82e340a2c9928b614e2eb
PAOLO PC <PAOLO PC@Pololopc>
2021-06-03 19:19:23 -0500

    JOptionPane(paolo)

--- github/src/main/java/com/mycompany/github/proyecto1.java
+++ github/src/main/java/com/mycompany/github/proyecto1.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.github;
 
 import com.sun.source.tree.TryTree;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,117 +15,99 @@ public class proyecto1 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        int opcion = 0;
+       String  opcion;
 
         try {
                                                                                                             //menus
-            System.out.println("seleccione la operacion a realzar\n 1.-Area y permetro del Cuadrado.");
-            System.out.println("2.-Area y permetro del Rectangulo.");
-            System.out.println("3.-Area y permetro del Triángulo.");
-            System.out.println("4.-Area y permetro del Rombo.");
-            System.out.println("5.-Area y permetro del Romboide.");
-            System.out.println("6.-Area y permetro del Trapecio.");
-            System.out.println("7.-Area y permetro del Circulo.");
-            System.out.println("8.-Area y permetro del Poligono.");
-            opcion = sc.nextInt();
+        
+       opcion=JOptionPane.showInputDialog("selecione operacion a realizar."
+        +"\n 1.  cuadradro "
+        +"\n 2.  cuadradro"
+        +"\n 3. triangulo"
+        +"\n 4.rombo"
+        +"\n 5.romboide "
+        +"\n 6.trapecio"
+        +"\n 7.circulo"
+        +"\n 8.salir" );
 
             switch (opcion) {
-                case 1:                                                         //cuadrado
-                    System.out.println("ingrese el lado");
-                    lado = sc.nextDouble();
-                    Cuadrados cua = new Cuadrados(lado);
+                case "1":                                                         //cuadrado
+                  lado=Double.parseDouble(JOptionPane.showInputDialog("ingrese los lados "));   
+                  Cuadrados cua = new Cuadrados(lado);
                     cua.area();
                     cua.perimetro();
                     break;
 
-                case 2:                                                         //rectangulo
-                    System.out.println("ingrese la base");
-                    base = sc.nextDouble();
-                    System.out.println("ingrese la altura");
-                    altura = sc.nextDouble();
-                    Rectangulo rec = new Rectangulo(base, altura);
+                case "2":                                                         //rectangulo
+                   
+                    base=Double.parseDouble(JOptionPane.showInputDialog("ingrese la base"));    
+                    altura=Double.parseDouble(JOptionPane.showInputDialog("ingrese la altura"));
+                   Rectangulo rec = new Rectangulo(base, altura);
                     rec.area();
                     rec.perimetro();
                     break;
-                case 3:                                                         //traingulo
+                case "3":                                                         //traingulo
                     double suma = 0;
-                    System.out.println("Ingrese la Altura");
-                    altura = sc.nextDouble();
-                    System.out.println("Ingrese la Base");
-                    base = sc.nextDouble();
-                    System.out.println("Ingrese un Lado");
-                    lado = sc.nextDouble();
+                    altura=Double.parseDouble(JOptionPane.showInputDialog("ingrese la Altura"));
+                    base=Double.parseDouble(JOptionPane.showInputDialog("ingrese la base"));
+                    lado=Double.parseDouble(JOptionPane.showInputDialog("ingrese valor para  un lado "));
                     Triangulo tri = new Triangulo(altura, base, lado);
                     tri.area();
                     tri.perimetro();
 
                     break;
-                case 4:                                                         //rombo
-                    System.out.println("Ingrese el Diametro Mayor");
-                    Diam=sc.nextDouble();
-                    System.out.println("Ingrese el Diametro Menor");
-                    diam=sc.nextDouble();
-                    System.out.println("Ingrese el Lado");
-                    lado=sc.nextDouble();
+                case "4":            //rombo
+                    Diam=Double.parseDouble(JOptionPane.showInputDialog("Ingrese Diametro Mayor "));
+                    diam=Double.parseDouble(JOptionPane.showInputDialog("Ingrese Diametro Menor"));
+                    lado=Double.parseDouble(JOptionPane.showInputDialog("Ingrese Lado "));
                     Rombo rom = new Rombo(Diam, diam, lado);
                     rom.area();
                     rom.perimetro();
                     break;
-                case 5:                                                         //romboide
-                    System.out.println("Ingrese la Base ");
-                    base=sc.nextDouble();
-                    System.out.println("Ingrese la Altura ");
-                    altura=sc.nextDouble();
+                case "5":                                                         //romboide
+                  base=Double.parseDouble(JOptionPane.showInputDialog("Ingrese La Base"));
+                  altura=Double.parseDouble(JOptionPane.showInputDialog("Ingrese La Altura"));
                     Romboide romboi = new Romboide(base,altura);
                     romboi.area();
                     romboi.perimetro();
                     break;
-                case 6:                                                         //trapecio
-                    System.out.println("Ingrese el Base Mayor");
-                    diam = sc.nextDouble();
-                    System.out.println("Ingrese el base Menor");
-                    Diam = sc.nextDouble();
-                    System.out.println("Ingrese la Altura");
-                    altura = sc.nextDouble();
-                    System.out.println("Ingrese El Lado");
-                    lado = sc.nextDouble();
+                case "6":     //trapecio
+                    diam=Double.parseDouble(JOptionPane.showInputDialog("Ingrese la  Base Mayor"));
+                    Diam=Double.parseDouble(JOptionPane.showInputDialog("Ingrese la  Base Menor"));
+                    altura=Double.parseDouble(JOptionPane.showInputDialog("Ingrese la Altura"));
+                    lado=Double.parseDouble(JOptionPane.showInputDialog("Ingrese El Lado"));
                     Trapecio tra = new Trapecio(diam, Diam, altura, lado);
                     tra.area();
                     tra.perimetro();
                     break;
-                case 7:                                                         //circulo
-                    System.out.println("Ingrese el Diametro");
-                    Diam=sc.nextDouble();
-                    System.out.println("Ingrese el Radio");
-                    radio=sc.nextDouble();
+                case "7":   //circulo
+                    Diam=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el Diametro"));
+                    radio=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el Radio"));
                     Circulo cir = new Circulo(Diam,radio);
                     cir.area();
                     cir.perimetro();
                     break;
-                case 8:                                                         //poligono
-                    System.out.println("Ingrese el Numero de Lados");
-                    numla=sc.nextDouble();
-                    System.out.println("Ingrese el Valor Del Lado");
-                    lado=sc.nextDouble();
-                    System.out.println("Ingrese La Apotema");
-                    apotema=sc.nextDouble();
+                case "9":                                                         //poligono
+                    numla=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el Numero de Lados "));
+                    lado=Double.parseDouble(JOptionPane.showInputDialog("Ingrese Valor para Lado"));
+                    apotema=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el Apotema"));
                     Poligonon pol = new Poligonon(apotema,numla,lado);
                     pol.perimetro();
                     pol.area();
                     break;
             }                                                                   //Exepciones
         } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir para cero.");
+            JOptionPane.showMessageDialog(null, "No se puede dividir para cero.");
             System.out.println(e.getMessage());
             e.printStackTrace();
         } catch (Exception e) {
-
-            System.out.println("Error comuniquese con TIC's");
+            JOptionPane.showMessageDialog(null, "Error comuniquese con TIC's.");
+           
             System.out.println(e.getMessage());
             e.printStackTrace();
         } finally {
             //cerrar la conexion 
-            System.out.println("Grcias por utilizar nuestro sericio.");
+             JOptionPane.showMessageDialog(null,"Grcias por utilizar nuestro sericio.");
         }
 
     }
